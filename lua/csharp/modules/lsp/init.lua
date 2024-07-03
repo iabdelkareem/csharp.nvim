673ce12cb9d73f3014f4e434e2c0995926cdef3f
Jonathan Dent <jon@jdent.co.uk>
2024-07-04 00:29:44 +0100

    fix: getting intial roslyn support working

--- lua/csharp/modules/lsp/init.lua
+++ lua/csharp/modules/lsp/init.lua
@@ -1,22 +1,78 @@
 local M = {}
 local config_store = require("csharp.config")
 
---- @return string
---- @param buffer number
-local function get_root_dir(buffer)
-  local file_name = vim.api.nvim_buf_get_name(buffer)
+---@param file_paths string[]
+---@param search_string string
+---@return string?
+local function get_filepath_containing_string(file_paths, search_string)
+    for _, file_path in ipairs(file_paths) do
+        local file = io.open(file_path, "r")
 
-  if file_name:sub(-#"csx") == "csx" then
-    return file_name:match(".*/")
-  end
+        if not file then
+            return nil
+        end
 
-  local root_dir = vim.fn.systemlist("fd -e sln . " .. vim.loop.cwd())[1]
+        local content = file:read("*a")
+        file:close()
 
-  if root_dir == nil then
-    root_dir = vim.loop.cwd()
-  end
+        if content:find(search_string, 1, true) then
+            return file_path
+        end
+    end
 
-  return root_dir
+    return nil
+end
+
+---@param buffer integer
+---@return string?
+function M.get_sln_directory(buffer)
+    return vim.fs.root(buffer, function(name)
+        return name:match("%.sln$") ~= nil
+    end)
+end
+
+---@param buffer integer
+---@return string[]?
+function M.get_all_sln_files(buffer)
+    local sln_dir = M.get_sln_directory(buffer)
+    if not sln_dir then
+        return nil
+    end
+
+    return vim.fn.glob(vim.fs.joinpath(sln_dir, "*.sln"), true, true)
+end
+
+--- Find a path to sln file that is likely to be the one that the current buffer
+--- belongs to. Ability to predict the right sln file automates the process of starting
+--- LSP, without requiring the user to invoke CSTarget each time the solution is open.
+--- The prediction assumes that the nearest csproj file (in one of parent dirs from buffer)
+--- should be a part of the sln file that the user intended to open.
+---@param buffer integer
+---@return string?
+function M.predict_sln_file(buffer)
+    local sln_files = M.get_all_sln_files(buffer)
+
+    if not sln_files then
+        return nil
+    end
+
+    local csproj_dir = vim.fs.root(buffer, function(name)
+        return name:match("%.csproj$") ~= nil
+    end)
+
+    if not csproj_dir then
+        return nil
+    end
+
+    local csproj_files = vim.fn.glob(vim.fs.joinpath(csproj_dir, "*.csproj"), true, true)
+
+    if #csproj_files > 1 then
+        return nil
+    end
+
+    local csproj_filename = vim.fn.fnamemodify(csproj_files[1], ":t")
+
+    return get_filepath_containing_string(sln_files, csproj_filename)
 end
 
 function M.setup()
@@ -30,11 +86,11 @@ function M.setup()
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "cs",
     callback = function(args)
-      local solution_or_root_dir = get_root_dir(args.buf)
+      local solution_or_root_dir = M.predict_sln_file(args.buf)
       if config.use_omnisharp then
         require("csharp.modules.lsp.omnisharp").start_omnisharp(args.buf, solution_or_root_dir)
       else
-        require("csharp.modules.lsp.roslyn").start_roslyn(args.buf, solution_or_root_dir)
+        require("csharp.modules.lsp.roslyn").start_roslyn(solution_or_root_dir, config.cmd_path)
       end
     end,
     group = lsp_group,
--- lua/csharp/modules/lsp/roslyn.lua
+++ lua/csharp/modules/lsp/roslyn.lua
@@ -1,51 +1,131 @@
 local M = {}
-local config_store = require("csharp.config")
-local logger = require("csharp.log")
 
-function M.start_roslyn(buffer, root)
-  vim.lsp.set_log_level(vim.log.levels.DEBUG)
-  local config = config_store.get_config().lsp
+---@param handler function
+local function with_filtered_watchers(handler)
+    return function(err, res, ctx, config)
+        for _, reg in ipairs(res.registrations) do
+            if reg.method == vim.lsp.protocol.Methods.workspace_didChangeWatchedFiles then
+                local watchers = vim.tbl_filter(function(watcher)
+                    if type(watcher.globPattern) == "table" then
+                        local base_uri = nil ---@type string?
+                        if type(watcher.globPattern.baseUri) == "string" then
+                            base_uri = watcher.globPattern.baseUri
+                            -- remove trailing slash if present
+                            if base_uri:sub(-1) == "/" then
+                                watcher.globPattern.baseUri = base_uri:sub(1, -2)
+                            end
+                        elseif type(watcher.globPattern.baseUri) == "table" then
+                            base_uri = watcher.globPattern.baseUri.uri
+                            -- remove trailing slash if present
+                            if base_uri:sub(-1) == "/" then
+                                watcher.globPattern.baseUri.uri = base_uri:sub(1, -2)
+                            end
+                        end
 
-  local cmd = {
-    "dotnet",
-    config.cmd_path,
-    "--logLevel",
-    "Debug",
-    "--extensionLogDirectory",
-    "/home/ibrahim",
-  }
+                        if base_uri ~= nil then
+                            local base_dir = vim.uri_to_fname(base_uri)
+                            -- use luv to check if baseDir is a directory
+                            local stat = vim.loop.fs_stat(base_dir)
+                            return stat ~= nil and stat.type == "directory"
+                        end
+                    end
 
-  logger.debug("Starting roslyn: ", { feature = "roslyn", cmd = cmd, buffer = buffer, root = root })
+                    return true
+                end, reg.registerOptions.watchers)
 
-  local client_id = vim.lsp.start({
-    name = "roslyn",
-    cmd = cmd,
-    root_dir = vim.fs.dirname(root),
-    capabilities = config.capabilities,
-    on_init = function(client)
-      local solution = vim.uri_from_fname(root)
-      logger.debug("Roslyn: Executing on_init", { solution = solution })
-      client.notify("solution/open", { solution = solution })
-    end,
-    on_error = function(code, ...)
-      logger.error("Error launch roslyn", { code = code, ... })
-    end,
-    on_attach = function(client, buffer)
-      logger.debug("Attaching to roslyn", { buffer = buffer })
-    end,
-    handlers = {
-      ["workspace/projectInitializationComplete"] = function()
-        logger.debug("Roslyn project initialization complete")
-      end,
-      ["workspace/_roslyn_projectHasUnresolvedDependencies"] = function()
-        logger.debug("Roslyn project has unresolved dependencies")
-      end,
-    },
-  }, {
-    bufnr = buffer,
-  })
+                reg.registerOptions.watchers = true and watchers or {}
+            end
+        end
+        return handler(err, res, ctx, config)
+    end
+end
 
-  logger.debug("Roslyn launched client", { client_id = client_id })
+---@param pipe string
+---@param target string
+local function lsp_start(pipe, target)
+    local config = {}
+    config.name = "roslyn"
+    config.cmd = vim.lsp.rpc.connect(pipe)
+    config.root_dir = vim.fs.dirname(target)
+    config.on_init = function(client)
+        vim.notify("Initializing Roslyn client for " .. target, vim.log.levels.INFO)
+        client.notify("solution/open", {
+            ["solution"] = vim.uri_from_fname(target),
+        })
+    end
+    config.handlers = {
+        ["client/registerCapability"] = with_filtered_watchers(
+            vim.lsp.handlers["client/registerCapability"]
+        ),
+        ["workspace/projectInitializationComplete"] = function()
+            vim.notify("Roslyn project initialization complete", vim.log.levels.INFO)
+        end,
+        ["workspace/_roslyn_projectHasUnresolvedDependencies"] = function()
+            vim.notify("Detected missing dependencies. Run dotnet restore command.", vim.log.levels.ERROR)
+            return vim.NIL
+        end,
+    }
+
+    local client_id = vim.lsp.start(config)
+
+    -- Handle the error in some way
+    if not client_id then
+        return
+    end
+
+    local client = vim.lsp.get_client_by_id(client_id)
+    if not client then
+        return
+    end
+
+    --  local commands = require("roslyn.commands")
+    --commands.fix_all_code_action(client)
+    --commands.nested_code_action(client)
+end
+
+---@param exe string
+---@param target string
+local function run_roslyn(exe, target)
+    local cmd = {
+        "dotnet",
+        exe,
+        "--logLevel=Information",
+        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+    }
+
+    vim.system(cmd, {
+        detach = not vim.uv.os_uname().version:find("Windows"),
+        stdout = function(_, data)
+            if not data then
+                return
+            end
+
+            -- try parse data as json
+            local success, json_obj = pcall(vim.json.decode, data)
+            if not success then
+                return
+            end
+
+            local pipe_name = json_obj["pipeName"]
+            if not pipe_name then
+                return
+            end
+
+            vim.schedule(function()
+                lsp_start(pipe_name, target)
+            end)
+        end,
+        stderr_handler = function(_, chunk)
+            local log = require("vim.lsp.log")
+            if chunk and log.error() then
+                log.error("rpc", "dotnet", "stderr", chunk)
+            end
+        end,
+    })
+end
+
+function M.start_roslyn(root, exe)
+   run_roslyn(exe, root)
 end
 
 return M
