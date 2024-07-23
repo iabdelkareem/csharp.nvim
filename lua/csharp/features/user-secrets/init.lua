4aa782eede8bbccc8d13fdf7f4093366c5ea2917
João Martins <joaoantoniomartinsfilho@gmail.com>
2024-07-22 22:20:08 -0300

    ♻️ Refactor as code review in pull request

--- lua/csharp/features/user-secrets/init.lua
+++ lua/csharp/features/user-secrets/init.lua
@@ -2,16 +2,20 @@ local M = {}
 local logger = require("csharp.log")
 local utils = require("csharp.utils")
 local dotnet_cli = require("csharp.modules.dotnet-cli")
+local _feature_name = "user-secrets"
 
 -- Creates the user secret file if the same
 -- doesn't exists
-local function _ensure_secret_exists(user_secret_file_path)
-  local file, _ = io.open(user_secret_file_path, "r+")
+local function _ensure_secret_exists(user_secret_folder_path)
+  local full_file_path = user_secret_folder_path .. "/secrets.json"
+  local file, _ = io.open(full_file_path, "r+")
 
   if not file then
-    os.execute("mkdir -p " .. user_secret_file_path)
-    os.execute("chmod 600 " .. user_secret_file_path)
+    os.execute("mkdir -p " .. user_secret_folder_path)
+    os.execute("echo \"{\n}\" > " .. full_file_path)
   end
+
+  return full_file_path
 end
 
 -- Paramter:
@@ -20,7 +24,7 @@ end
 -- Returns a string with the user id or nil.
 --- @param output_init_command string
 --- @return string | nil
-local function _extract_user_id_from_command(output_init_command)
+local function _extract_user_id_from_command_output(output_init_command)
   return string.match(output_init_command, "Set UserSecretsId to '(.*)'")
 end
 
@@ -29,7 +33,7 @@ local function _extract_user_id_from_project(project_path)
   local user_id
 
   if not project_file then
-    logger.error("Something went wrong during read the project file", { feature = "user-secrets" })
+    logger.error("Something went wrong during read the project file", { feature = _feature_name })
     return
   end
 
@@ -52,36 +56,42 @@ end
 local function _init_secret(project_path)
   local output, _ = dotnet_cli.user_secrets("init", project_path)
 
+  logger.info("Output: ", { data = output })
+
   if output then
-    return _extract_user_id_from_command(output)
+    return _extract_user_id_from_command_output(output)
   end
 end
 
 local function _open_secret_in_buffer(user_secret_id)
-  local secret_file_path = os.getenv("HOME") .. "/.microsoft/usersecrets/" .. user_secret_id .. "/secrets.json"
-  _ensure_secret_exists(secret_file_path)
+  local secret_folder_path = os.getenv("HOME") .. "/.microsoft/usersecrets/" .. user_secret_id
+  local file_path = _ensure_secret_exists(secret_folder_path)
 
-  vim.cmd.edit(secret_file_path)
+  vim.cmd.edit(file_path)
 end
 
 local function _open_secret()
   local project_information = require("csharp.features.workspace-information").select_project()
 
   if not project_information then
-    logger.error("No project selected", { feature = "user-secrets" })
+    logger.error("No project selected", { feature = _feature_name })
     return
   end
 
   local user_secret_id = _extract_user_id_from_project(project_information.Path)
 
   if not user_secret_id then
+    logger.warn("User Secret Id not found in project, creating...", { feature = _feature_name })
+
     user_secret_id = _init_secret(project_information.Path)
 
     if not user_secret_id then
-      logger.error("Something went wrong during user secrets creation", { feature = "user-secrets" })
+      logger.error("Something went wrong during user secrets creation", { feature = _feature_name })
       return
     end
   end
+
+  logger.info("Opening user secret", { feature = _feature_name })
 
   _open_secret_in_buffer(user_secret_id)
 end
